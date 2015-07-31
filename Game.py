809f91d0b6acad868908644f58aa97606325baa5
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-07-31 12:40:17 +0530

    Ready

---
+++ Game.py
@@ -0,0 +1,12 @@
+"""
+You're really on your own here. Now, your job is to make a program that implements
+your strategy to play Tic-Tac-Toe. Pick any strategy you want. 
+
+After all, you'll be playing against the strategy.
+
+"""
+
+from random import randint; # You can remove this if you don't need it.
+# randint(a, b) will return a random integer in the set {a, ... , b}
+
+from ExtendedAPI import *; # Don't remove this, though.
