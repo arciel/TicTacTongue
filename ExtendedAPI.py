d688732b94c1da85925f5f4b559a19bce94efc55
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-07-31 12:51:06 +0530

    Ready

--- ExtendedAPI.py
+++ ExtendedAPI.py
@@ -7,4 +7,21 @@ here, for Game.py.
 
 from Library import *; # Don't remove!
 
+# You may need this method:
+
+def printToConsole():
+    """ This method prints the Grid onto the screen."""
+    for i in range(0, 3):
+        print("   |   |   ")
+        for j in range(0, 3):
+            print "\b " + str(Grid[i][j]) + " ",
+            if (j != 2):
+                print "\b|",
+            else:
+                print ""
+        if (i != 2):
+            print "___|___|___"
+        else:
+            print "   |   |   "
+
 # After doing that, head to the showdown. Game.py.
