e10815bd3874576eb29043fe8037031a21f23452
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-07-31 09:56:56 +0530

    More comments.

--- Functions.py
+++ Functions.py
@@ -42,14 +42,14 @@ Grid = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
 
 def crossesInRowZero():
     """This function returns the number of Xs in row zero (0).""" 
-    count = 0                          # This will count the number of X's in the row.
-    index = 0                                # This is the column index in row 0. 
-    while (index < 3):                           # While loop starts.   <------<---   
-        if (Grid[0][index] == 'X'):             # If there's an X at (0, index) ... ^
-            count = count + 1                   # ... increase count by 1.           ^
-        else:                              # If there isn't an X at (0, index) ...   ^
-            pass                           # ... do nothing.                         ^ 
-        index = index + 1             # Either way, now look at the next box.        ^
+    count = 0                           #This will count the number of X's in the row.
+    index = 0                                 #This is the column index in row 0. 
+    while (index < 3):                            #While loop starts.   <------<---   
+        if (Grid[0][index] == 'X'):              #If there's an X at (0, index) ... ^
+            count = count + 1                   #... increase count by 1.            ^
+        else:                                 #If there isn't an X at (0, index) ... ^
+            pass                            #... do nothing.                         ^ 
+        index = index + 1                  #Either way, now look at the next box.    ^
              # Follow this arrow for the next iteration.  ---->----------->---------^ 
     
     # We've looked at all boxes in row 0. Now index = 3. We're out of the loop.
