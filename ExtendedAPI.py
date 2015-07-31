6b61f02b448476fda068450c724ae369785e5134
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-07-31 12:19:29 +0530

    ExtendedAPI.py completed (it's small)

---
+++ ExtendedAPI.py
@@ -0,0 +1,10 @@
+"""
+Firstly, implement ALL the functions from Functions.py using isCross 
+(if they need it). After that, you can add pretty much any function you need
+here, for Game.py.
+
+"""
+
+import Library.py; # Don't remove!
+
+# After doing that, head to the showdown. Game.py.
