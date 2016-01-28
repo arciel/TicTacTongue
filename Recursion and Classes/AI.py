8bb18fa3829b3bd4d4bc1b2ac37894ee12a01b1a
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2016-01-28 05:25:31 -0600

    Working unbeatable algorithm. >:)

--- Recursion and Classes/AI.py
+++ Recursion and Classes/AI.py
@@ -1,12 +1,46 @@
-from Gamestate import Gamestate
+from Gamestate import *
 
-Grid = [['X', ' ', ' '], [' ', '0', ' '], [' ', ' ', ' ']]
+Grid = [[' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']]
 
-state = Gamestate(Grid, True)
+x_moves = raw_input("Welcome, TicTacMan/Woman! >:( Do you want to go first? (Y or N): ")
 
-state.make_states()
+zeros_move = True if (x_moves == 'N' or x_moves == 'n') else False
 
-state.calculate_value()
+state = Gamestate(Grid, zeros_move)
 
-print("State value:"),
-print(state.value)
+state.make_states()                                         # Generate the game tree.
+
+state.calculate_value()                                     # Compute each state's value.
+
+while (not game_end(state.grid)):
+    if (not zeros_move):
+        i = int(raw_input("Enter X in row __: "))
+        j = int(raw_input("Enter X in col __: "))
+        while (i < 0 or i > 2):
+            i = int(raw_input("Row should be between 0 and 2: "))
+        while (j < 0 or j > 2):
+            j = int(raw_input("Col should be between 0 and 2: "))
+        while (isCross(Grid, i, j) or isZero(Grid, i, j)):
+            print "Please enter unoccupied coordinates"
+            i = int(raw_input("Enter X in row __: "))
+            j = int(raw_input("Enter X in col __: "))
+        for new_state in state.states:
+            if (new_state.grid[i][j] == 'X'):
+                state = new_state
+                break
+        print("\nYou: ")
+    else:
+        state = state.states[0]
+        print("\nEarth's Mightiest Zeros: ")
+    printToConsole(state.grid)
+    zeros_move = not zeros_move
+        
+print("\n\nGame over:"),
+if (ttp_lost(state.grid)):
+    print("The Earth's Mightiest Zeros win!")
+elif (zeros_lost(state.grid)):
+    print("TicTacMan/Woman wins!")
+else:
+    print("Draw.")
--- Recursion and Classes/Gamestate.py
+++ Recursion and Classes/Gamestate.py
@@ -4,32 +4,38 @@ from ExtendedAPI import *
 
 class Gamestate:
     """The class implementing a state of the game."""
-    def __init__(self, grid, whos_turn):
+    def __init__(self, grid, zeros_move):
         """Constructor, especially useful for generating states recursively."""
         self.grid = grid
-        self.zeros = whos_turn
+        self.zeros_move = zeros_move
         self.states = []
         self.value = 0.0
 
     def make_states(self):
         """Makes child states of self."""
+        if (game_end(self.grid)) : return
         for i in range(3):
             for j in range(3):
                 if (self.grid[i][j] == ' '):
                     new_grid = [row[:] for row in self.grid]
-                    new_grid[i][j] = '0' if (self.zeros) else 'X'
-                    new_state = Gamestate(new_grid, not self.zeros)
+                    new_grid[i][j] = '0' if (self.zeros_move) else 'X'
+                    new_state = Gamestate(new_grid, not self.zeros_move)
                     self.states.append(new_state)
                     new_state.make_states()
+       
 
     def calculate_value(self):
         """Calculates the value of self."""
         if (not self.states):                                        # If self doesn't have any child states (is a leaf) ...
-            self.value = 10.0 if (not lost(self.grid)) else 0.0      # ... then assign a value depending upon the winner.
+            if (ttp_lost(self.grid)):                                           # ... assign it a value based on who won.
+                self.value = 10
+            elif (zeros_lost(self.grid)):
+                self.value = -10
+            else:
+                self.value = 0
         else:
             for state in self.states:
                 state.calculate_value()
                 self.value += state.value
             self.value //= len(self.states)
-                
-                    
+            self.states.sort(lambda a, b: -1 if (a.value > b.value) else (1 if (a.value < b.value) else 0))
